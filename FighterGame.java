a54b7211122f2b2ca603996af086f47eb5c24f40
Kod-Draken <m.dackhammar@gmail.com>
2024-11-05 10:12:57 +0100

    lots of features

--- FighterGame.java
+++ FighterGame.java
@@ -1,5 +1,34 @@
+import java.util.concurrent.ThreadLocalRandom;
+
 public class FighterGame {
     // maybe 1 more class for output
     Player player1 = new Player("Mattias");
     Player player2 = new Player("Emilia");
+
+    public void play() {
+        player1.setWeapon();
+    
+        player1.attack(player2);
+        System.out.println("player 2 hp: " + player2.getHitPoints());
+        player2.heal();
+        System.out.println("player 2 hp: " + player2.getHitPoints());
+
+        while(player1.getHitPoints() > 0 || player2.getHitPoints() > 0){
+            round();
+    
+        }
+    }
+
+    private void round(){
+        int whoStarts = ThreadLocalRandom.current().nextInt(0,1);
+        if (whoStarts == 1) {
+            player1.attack(player2);
+            player2.attack(player1);
+        }
+    }
+
+    private void playAgain(){
+        System.out.println("Vill du spela igen? Y/N");
+        play();
+    }
 }
